58f2e4928c768f85b03623ed7f73ae7944b40dcc
noahgabriel1997 <81257812+noahgabriel1997@users.noreply.github.com>
2021-04-12 13:26:45 -0500

    Update m_cal.js

--- mediation_cal/m_cal.js
+++ mediation_cal/m_cal.js
@@ -3,35 +3,25 @@ const container = document.querySelector('.container')
 const days = document.querySelectorAll('.day:not(yes)')
 const score = document.getElementById('score')
 
-// populating the UI
-
-// popUI()
-
-
 
 // functions
 
 function updateDayCount() {
     const yesDays = document.querySelectorAll('.day.yes')
     const yesDaysCount = yesDays.length
-    console.log(yesDaysCount)
     score.innerText = yesDaysCount
-
-    // saving to LS
-    const daysIndex = [...yesDays].map(day => [...days].indexOf(day))
-    localStorage.setItem('selectedDays', JSON.stringify(daysIndex))
 
 }
 
 // ELs
 
 container.addEventListener('click', (e) =>  {
-    
         e.target.classList.toggle('yes') 
-
         updateDayCount()
 })
 
 
 // running the function at page load so we don't get 0 as the count
+// I start with 5 days logged just to show colors 
+    // but could just store via LS
 updateDayCount()
